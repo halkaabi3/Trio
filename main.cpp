6485f3009b500100f9fffdb5ad2ffa6f93a82f9e
halkaabi3 <35825243+halkaabi3@users.noreply.github.com>
2018-04-18 15:23:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+//Author: Helal Alkaabi
+#include <string>
 #include <iostream>
+
 using namespace std;
 
+void greatestToLeast(int&,int&,int&);
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -8,9 +12,9 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
+   
 
-
-
+    greatestToLeast(red,green,blue);
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -19,3 +23,26 @@ int main()
   return 0;
   //...END OF "DO NOT CHANGE" AREA
 }
+  void greatestToLeast(int&Red,int&Green,int&Blue)
+  {
+    if(Red<Green)
+    {
+        int tmp=Red;
+        Red=Green;
+        Green=tmp;
+    }
+    if(Red<Blue)
+    {
+        int tmp=Red;
+        Red=Blue;
+        Blue=tmp;
+    }
+    if(Green<Blue)
+    {
+        int tmp=Green;
+        Green=Blue;
+        Blue=tmp;
+
+    }
+    return ;
+  }
